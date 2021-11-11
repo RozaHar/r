3ddc0ef9e0a47d23e6d10fc4df945e5ad4252403
RozaHar <rozaharutyuny.129@gmail.com>
2021-11-11 00:46:44 -0800

    Added flash massages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
 	has_rich_text :content
 	has_many :article_tags, dependent: :destroy
-	has_many :comments
+	has_many :comments, dependent: :destroy
+	validates :hidline,presence: true
 	accepts_nested_attributes_for :article_tags
 end
